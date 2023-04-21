a23109b5f75df39516fc94349b2da1cff3717dbe
Duy Hung <dongocduyhung7@gmail.com>
2023-04-21 14:48:20 +0700

    setup Router

---
+++ src/components/Headers/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <h2>Hung shop</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
